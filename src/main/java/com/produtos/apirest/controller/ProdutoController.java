e451e46e3c922bc27c2891129e79419fd431eec1
Eduardo Florêncio de Freitas <eduardoflorencio96@gmail.com>
2020-01-24 17:57:50 -0300

    API REST com Spring Boot - parte 4: Implementando os métodos DELETE e PUT

--- src/main/java/com/produtos/apirest/controller/ProdutoController.java
+++ src/main/java/com/produtos/apirest/controller/ProdutoController.java
@@ -3,9 +3,11 @@ package com.produtos.apirest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,14 +37,23 @@ public class ProdutoController {
 		return produtoRepository.findById(id).get();
 	}
 	
-	// Insere um produto no banco de dados
+	// Insere um produto
 	@PostMapping("/produto")
 	public Produto salvaProduto(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
 	}
 	
+	// Atualiza um produto
+	@PutMapping("/produto")
+	public Produto atualizaProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
 	
-	
+	// Deleta um produto
+	@DeleteMapping("/produto")
+	public void deletaProduto(@RequestBody Produto produto) {
+		produtoRepository.delete(produto);
+	}
 	
 	
 }
